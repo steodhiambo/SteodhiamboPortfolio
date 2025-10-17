fbb13d2231202353e1245c31c7fd3a6dcaad1597
steodhiambo <stephenoginga6@gmail.com>
2025-10-17 14:20:37 +0300

    Fix: Add missing project images for projects 1, 2, and 3
    - Import screenshot_project_1.png, screenshot_project_2.png, screenshot_project_3.png
    - Add image mappings in ProjectItem component
    - Resolves issue where projects 5, 6, 7 images were not displaying

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import atm from "../assets/atm.png";
+import Ascii from "../assets/Ascii.png";
+import groupie from "../assets/groupie.png";
+import portfolio from "../assets/portfolio.png";
+import screenshot1 from "../assets/screenshot_project_1.png";
+import screenshot2 from "../assets/screenshot_project_2.png";
+import screenshot3 from "../assets/screenshot_project_3.png";
+
+const imageMap = {
+  "../assets/atm.png": atm,
+  "../assets/Ascii.png": Ascii,
+  "../assets/groupie.png": groupie,
+  "../assets/portfolio.png": portfolio,
+  "../assets/screenshot_project_1.png": screenshot1,
+  "../assets/screenshot_project_2.png": screenshot2,
+  "../assets/screenshot_project_3.png": screenshot3
+};
 
 function ProjectItem({ image, name, id }) {
   const navigate = useNavigate();
+  const actualImage = imageMap[image] || image;
+  
   return (
-    <div
-      className="projectItem"
-      onClick={() => {
-        navigate("/project/" + id);
-      }}
-    >
-      <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
-      <h1> {name} </h1>
+    <div className="projectItem" onClick={() => navigate("/project/" + id)}>
+      <div className="bgImage" style={{ backgroundImage: `url(${actualImage})` }} />
+      <h1>{name}</h1>
     </div>
   );
 }
