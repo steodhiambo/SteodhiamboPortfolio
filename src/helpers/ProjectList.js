e49539b162e6e082a5362b40e44c621a5e5454a1
steodhiambo <stephenoginga6@gmail.com>
2025-10-17 14:21:38 +0300

    Update: Footer component and ProjectList helper
    - Update Footer.js with enhanced functionality
    - Update ProjectList.js helper for project management

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,6 +2,9 @@ import atm from "../assets/atm.png";
 import Ascii from "../assets/Ascii.png";
 import groupie from "../assets/groupie.png";
 import portfolio from "../assets/portfolio.png";
+import screenshot1 from "../assets/screenshot_project_1.png";
+import screenshot2 from "../assets/screenshot_project_2.png";
+import screenshot3 from "../assets/screenshot_project_3.png";
 
 export const ProjectList = [
   {
@@ -12,17 +15,31 @@ export const ProjectList = [
   {
     name: "Ascii-Art- Web",
     image: Ascii,
-    skills: "HTML,Http header",
+    skills: "HTML,CSS,JavaScript,Http header",
   },
   {
     name: "Groupie-Tracker",
     image: groupie,
-    skills: "HTML,Node.js,JSON,API",
+    skills: "HTML,Node.js,JSON,API,REST API",
   },
   {
     name: "Portfolio Website",
     image: portfolio,
-    skills: "React,Node.js,HTML,CSS",
+    skills: "React,Node.js,HTML,CSS,JavaScript",
   },
-  
+  {
+    name: "New Project 1",
+    image: screenshot1,
+    skills: "To be updated",
+  },
+  {
+    name: "New Project 2",
+    image: screenshot2,
+    skills: "To be updated",
+  },
+  {
+    name: "New Project 3",
+    image: screenshot3,
+    skills: "To be updated",
+  },
 ];
