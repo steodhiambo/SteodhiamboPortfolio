3dd5df66521d3780b0f2cc25028737b95872322b
steodhiambo <stephenoginga6@gmail.com>
2025-10-20 13:46:42 +0300

    Fix ProjectItem.js image imports to match existing assets

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import atm from "../assets/atm.png";
-import Ascii from "../assets/Ascii.png";
-import groupie from "../assets/groupie.png";
 import portfolio from "../assets/portfolio.png";
 import netfixProject from "../assets/netfix-project.png";
 import movieDiscoveryApp from "../assets/movie-discovery-app.png";
 import umojaVoicesProject from "../assets/umoja-voices-project.png";
 
 const imageMap = {
-  "../assets/atm.png": atm,
-  "../assets/Ascii.png": Ascii,
-  "../assets/groupie.png": groupie,
   "../assets/portfolio.png": portfolio,
   "../assets/netfix-project.png": netfixProject,
   "../assets/movie-discovery-app.png": movieDiscoveryApp,
